0e021fe734c3ce727733d0abe8131aa753ed304e
Pramod Vidyagar <pramod.vidyagar@gmail.com>
2017-02-05 17:34:18 +0530

    prop file added

---
+++ main/login/PropFiles.java
@@ -0,0 +1,32 @@
+package login;
+
+public class PropFiles {
+	
+	public void test()
+	{
+		System.out.println("This is new Test for Git repository");
+	}
+
+	private static void showMessage(){
+		System.out.println("Test Passed");
+	}
+	
+	private static void showMessage2(){
+		System.out.println("Test Passed");
+	}
+	
+	private static void showMessage23(){
+		System.out.println("Test Passed");
+	}
+	
+	
+	public static void main(String[] args) {
+		LoginTest LT = new LoginTest();
+		LT.test();
+		showMessage();
+		System.out.println("Test Passed Successfully");
+		
+	}
+	
+	
+}
