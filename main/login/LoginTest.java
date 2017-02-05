8033e52f1c3a23f6186bcb78c6cb3ee4e879664d
Pramod Vidyagar <pramod.vidyagar@gmail.com>
2017-02-05 17:29:38 +0530

    display method added

--- main/login/LoginTest.java
+++ main/login/LoginTest.java
@@ -7,12 +7,15 @@ public class LoginTest {
 		System.out.println("This is new Test for Git repository");
 	}
 
+	private static void showMessage(){
+		System.out.println("Test Passed");
+	}
+	
 	public static void main(String[] args) {
 		LoginTest LT = new LoginTest();
 		LT.test();
+		showMessage();
 		System.out.println("Test Passed Successfully");
 		
-
 	}
-
 }
