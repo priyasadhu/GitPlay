c0f51e6d593f898067efd370d5f0ca6b80a10429
pramod vidyagar <pvidyagar@nisum.com>
2017-02-05 17:17:39 +0530

    New Funtion added

--- main/login/LoginTest.java
+++ main/login/LoginTest.java
@@ -1,9 +1,17 @@
 package login;
 
 public class LoginTest {
+	
+	public void test()
+	{
+		System.out.println("This is new Test for Git");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Test Passed");
+		LoginTest LT = new LoginTest();
+		LT.test();
+		System.out.println("Test Passed Successfully");
+		
 
 	}
 
